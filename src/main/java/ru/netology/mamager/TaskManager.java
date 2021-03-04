866f1917de1965c5655bb183a70eb3afc9a39f33
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-03-04 13:39:00 +0300

    method findBy updated

--- src/main/java/ru/netology/mamager/TaskManager.java
+++ src/main/java/ru/netology/mamager/TaskManager.java
@@ -50,58 +50,44 @@ public class TaskManager {
         return result;
     }
 
-    public Collection<Task> filterByAssignee(String... queries) {
+    public Collection<Task> filterByLabel(String... queries) {
         Set<String> querySet = new HashSet<>(Arrays.asList(queries));
         int requestCount = querySet.size();
         Set<Task> result = new HashSet<>();
-        int i = 0;
 
         for (Task item : getAll()) {
-            if (item.getAssignee().size() < querySet.size()) {   // если размер массива запроса превышает размер массива поля обьекта - сравнения не происходит
+            if (item.getLabel().size() < querySet.size()) {   // если размер массива запроса превышает размер массива поля обьекта - сравнения не происходит
                 continue;
             }
-            else if (item.getAssignee().size() > querySet.size()) {
-                List<String> q = new ArrayList<String>(querySet);
-                List<String> a = new ArrayList<String>(item.getAssignee());
+            else if (item.getLabel().size() > querySet.size()) {
+                List<String> q = new ArrayList<>(querySet);
+                List<String> a = new ArrayList<>(item.getLabel());
+                String e = "";
+                if (q.size() < a.size()) {
+                    q.add(e);
+                }
                 Collections.sort(q);
                 Collections.sort(a);
-                int t = 0;
+                int i = 0;
                 for(int j = 0; j < q.size(); j++){
                     for(int l = 0; l < q.size(); l++){
                         if(q.get(j).equals(a.get(l))){
-                            t++;
+                            i++;
                         }
                         }
                     }
-                if(t == requestCount) {
+                if(i == requestCount) {
                     result.add(item);
                 }
             }
-            else if (item.getAssignee().size() == querySet.size()) {
-                List<String> q = new ArrayList<String>(querySet);
-                List<String> a = new ArrayList<String>(item.getAssignee());
+            else if (item.getLabel().size() == querySet.size()) {   // если размер массива запроса равен размеру массива поля обьекта - происходит сравнение массивов
+                List<String> q = new ArrayList<>(querySet);
+                List<String> a = new ArrayList<>(item.getLabel());
                 Collections.sort(q);
                 Collections.sort(a);
-
                 if(q.equals(a)) {
                     result.add(item);
                 }
-
-                // если размер массива запроса равен размеру массива поля обьекта - сравнения не происходит
-//            for (String query : querySet) {
-//                Set<Task> tmp = new HashSet<>();
-//                    for (String n : item.getAssignee()) {
-//                        boolean isEqual = n.equalsIgnoreCase(query);
-//                        if (isEqual) {
-//                            i++;
-//                            tmp.add(item);
-//                        }
-//                        if (i == requestCount) {
-//                            result.addAll(tmp);
-//                            i = 0;
-//                        }
-//                    }
-//                }
             }
         }
         return result;
--- src/test/java/ru/netology/mamager/TaskManagerTest.java
+++ src/test/java/ru/netology/mamager/TaskManagerTest.java
@@ -13,11 +13,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TaskManagerTest {
     TaskManager manager = new TaskManager();
-    Task first = new Task(1, "Task1", "mike", Set.of("ann", "jim", "david", "kate"), Set.of("test"), true);
-    Task second = new Task(2, "Task2", "ann", Set.of("john", "jim"), Set.of("test"), false);
-    Task third = new Task(3, "Task3", "jim", Set.of("john"), Set.of("test"), true);
-    Task forth = new Task(4, "Task4", "mike", Set.of("jim", "ann"), Set.of("test"), true);
-    Task fifth = new Task(5, "Task5", "ann", Set.of("mike", "ann", "jim"), Set.of("test"), false);
+    Task first = new Task(1, "Task1", "mike", Set.of("ann", "jim", "david", "kate"), Set.of("one", "two"), true);
+    Task second = new Task(2, "Task2", "ann", Set.of("john", "jim"), Set.of("one", "two", "three"), false);
+    Task third = new Task(3, "Task3", "jim", Set.of("john"), Set.of("five", "one", "two"), true);
+    Task forth = new Task(4, "Task4", "mike", Set.of("jim", "ann"), Set.of("one"), true);
+    Task fifth = new Task(5, "Task5", "ann", Set.of("mike", "ann", "jim"), Set.of("one", "two", "four", "five"), false);
 
     @Nested
     public class addRemove {
@@ -62,31 +62,31 @@ public class TaskManagerTest {
         }
 
         @Test
-        public void shouldGetAssignee() {
-            Collection<Task> expected = manager.filterByAssignee("ann");
+        public void shouldGetLabel() {
+            Collection<Task> expected = manager.filterByLabel("one");
 
             System.out.println(expected);
 
         }
 
         @Test
-        public void shouldGetSomeAssignee() {
-            Collection<Task> expected = manager.filterByAssignee("ann", "jim");
+        public void shouldGetSomeLabel() {
+            Collection<Task> expected = manager.filterByLabel("one", "two");
 
             System.out.println(expected);
 
         }
         @Test
-        public void shouldGetNoneAssignee() {
-            Collection<Task> expected = manager.filterByAssignee("ann", "jim", "john");
+        public void shouldGetNoneLabel() {
+            Collection<Task> expected = manager.filterByLabel("one", "three", "five");
 
             System.out.println(expected);
 
         }
 
         @Test
-        public void shouldGetOneAssignee() {
-            Collection<Task> expected = manager.filterByAssignee("ann", "jim", "kate", "david");
+        public void shouldGetOneItemWithLabels() {
+            Collection<Task> expected = manager.filterByLabel("five", "one", "two", "four");
 
             System.out.println(expected);
 
