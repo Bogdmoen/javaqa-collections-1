8a2aa9666bc7d7e6e4e29e386c52c407ad51d3a0
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-03-04 06:01:25 +0300

    method findBy #1.2

--- src/main/java/ru/netology/mamager/TaskManager.java
+++ src/main/java/ru/netology/mamager/TaskManager.java
@@ -4,6 +4,8 @@ import ru.netology.domain.Task;
 import ru.netology.repository.TaskRepository;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 public class TaskManager {
@@ -49,44 +51,57 @@ public class TaskManager {
     }
 
     public Collection<Task> filterByAssignee(String... queries) {
-
-        int requestCount = queries.length;
         Set<String> querySet = new HashSet<>(Arrays.asList(queries));
+        int requestCount = querySet.size();
         Set<Task> result = new HashSet<>();
-        boolean isBreak = false;
         int i = 0;
-        int length = 0;
 
         for (Task item : getAll()) {
-
-            for (String query : querySet) {
-                Set<Task> tmp = new HashSet<>();
-                if(item.getAssignee().size() == queries.length) {
-                for (String n : item.getAssignee()) {
-                    boolean isEqual = n.equalsIgnoreCase(query);
-                    if (isEqual) {
-                        i++;
-                        tmp.add(item);
-                    }
-                    if (i == requestCount) {
-                        result.addAll(tmp);
-                        i = 0;
-                    }
-                }
-                }
-                else if(item.getAssignee().size() < queries.length) {
-                    isBreak = true;
-                }
-                else {
-                    for (String n : item.getAssignee()) {
-                        boolean isEqual = n.equalsIgnoreCase(query);
-                        if (isEqual) {
-                            result.add(item);
+            if (item.getAssignee().size() < querySet.size()) {   // если размер массива запроса превышает размер массива поля обьекта - сравнения не происходит
+                continue;
+            }
+            else if (item.getAssignee().size() > querySet.size()) {
+                List<String> q = new ArrayList<String>(querySet);
+                List<String> a = new ArrayList<String>(item.getAssignee());
+                Collections.sort(q);
+                Collections.sort(a);
+                int t = 0;
+                for(int j = 0; j < q.size(); j++){
+                    for(int l = 0; l < q.size(); l++){
+                        if(q.get(j).equals(a.get(l))){
+                            t++;
+                        }
                         }
                     }
+                if(t == requestCount) {
+                    result.add(item);
                 }
-                if (isBreak)
-                    break;
+            }
+            else if (item.getAssignee().size() == querySet.size()) {
+                List<String> q = new ArrayList<String>(querySet);
+                List<String> a = new ArrayList<String>(item.getAssignee());
+                Collections.sort(q);
+                Collections.sort(a);
+
+                if(q.equals(a)) {
+                    result.add(item);
+                }
+
+                // если размер массива запроса равен размеру массива поля обьекта - сравнения не происходит
+//            for (String query : querySet) {
+//                Set<Task> tmp = new HashSet<>();
+//                    for (String n : item.getAssignee()) {
+//                        boolean isEqual = n.equalsIgnoreCase(query);
+//                        if (isEqual) {
+//                            i++;
+//                            tmp.add(item);
+//                        }
+//                        if (i == requestCount) {
+//                            result.addAll(tmp);
+//                            i = 0;
+//                        }
+//                    }
+//                }
             }
         }
         return result;
---
+++ src/main/java/ru/netology/ru/netology/filter/Filters.java
@@ -0,0 +1,9 @@
+package ru.netology.ru.netology.filter;
+
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class Filters {
+
+
+}
